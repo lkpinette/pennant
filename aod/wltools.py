4bd6baeafb4275c8a0547f9d40da264d413c4fdf
Luke <luke@LPK2012.(none)>
2014-05-12 21:40:11 -0400

    Merge fdwordoftype and senttools into wltools.

--- aod/wltools.py
+++ aod/wltools.py
@@ -1,4 +1,35 @@
 import nltk, utils
+enstop = nltk.corpus.stopwords.words('english')
+
+def fdWordOfType(postags, doprint=False):
+    ''' gets freqDist for all words of a certain tag if not in stopwords
+        and is alpha
+    '''
+    awords = [item[0].lower() for item in bocl_tw if item[1] in postags and
+        item[0].lower() not in enstop and item[0].isalpha()]
+    afd = nltk.probability.FreqDist(awords)
+    an2pls = [(afd[word], word) for word in list(set(awords))]
+    an2pls.sort()
+    
+    if doprint == True:
+        for an in an2pls:
+            if an[0] > 11:
+                print '%s : %s' %(an[1], an[0])
+
+    return an2pls
+
+bocl_tagged = nltk.corpus.TaggedCorpusReader('tagged', '.*\.txt')
+bocl_tw = bocl_tagged.tagged_words()
+
+wlist = fdWordOfType(['NN', 'NNS'])
+
+def writeWL(wl, dest):
+    rawwl = open(dest, 'w')
+    for w in wl:
+        if w[0]>11:
+            rawwl.write(w[1] + ' : ' + str(w[0]) + '\n')
+        
+writeWL(wlist, 'wordlists/rawwl1.txt')
 
 def areInSameSent(l1, l2, corpus, tags=None):
     newlist = []
@@ -41,5 +72,4 @@ def newNCFile(l1, oldfile, newfile, ddir = 'wordlists/'):
             newfile.write(oldnlist[i] + ' : ' + clist[i] + '\n')
     newfile.close()
 
-newNCFile(utils.twords, 'rawwl.txt', 'rawwl2.txt')
-    
+newNCFile(utils.twords, 'rawwl1.txt', 'rawwl3.txt')
